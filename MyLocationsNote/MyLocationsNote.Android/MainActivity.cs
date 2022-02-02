1b6194c3eb02931e59684615a454228f293b7ed2
maximSologubov <95292495+maximSologubov@users.noreply.github.com>
2022-02-03 00:51:03 +0200

    add prism

--- MyLocationsNote/MyLocationsNote.Android/MainActivity.cs
+++ MyLocationsNote/MyLocationsNote.Android/MainActivity.cs
@@ -4,6 +4,8 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using Prism;
+using Prism.Ioc;
 
 namespace MyLocationsNote.Droid
 {
@@ -16,13 +18,24 @@ namespace MyLocationsNote.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            LoadApplication(new App());
+            LoadApplication(new App(new AndroidInitializer()));
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+    internal class AndroidInitializer : IPlatformInitializer
+    {
+        public AndroidInitializer()
+        {
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            //throw new NotImplementedException();
         }
     }
 }
--- MyLocationsNote/MyLocationsNote/App.xaml.cs
+++ MyLocationsNote/MyLocationsNote/App.xaml.cs
@@ -1,18 +1,40 @@
-﻿using System;
+﻿using Prism;
+using Prism.Ioc;
+using Prism.Unity;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace MyLocationsNote
 {
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
-        public App()
+        public App(IPlatformInitializer initializer = null) : base(initializer)
         {
-            InitializeComponent();
 
-            MainPage = new MainPage();
         }
 
+        #region --- Private fields
+
+       /* private ISettinngsManager _settinngsManager;
+        private IRepository repository;
+        private IDbService dbService;
+        private IRegistration registration;
+        private IAuthorization authorizationService;/
+
+        #endregion
+
+        #region --- Properties ---
+
+        /*public ISettinngsManager SettingsManager => _settinngsManager ??= Container.Resolve<SettingsManager>();
+        public IRepository Repository => repository ??= Container.Resolve<Repository>();
+        public IDbService DbService => dbService ??= Container.Resolve<DbService>();
+        public IRegistration Registration => registration ??= Container.Resolve<Registration>();
+        public IAuthorization AuthorizationService => authorizationService ??= Container.Resolve<Authorization>();*/
+
+        #endregion
+
+        #region --- Overrides ---
         protected override void OnStart()
         {
         }
@@ -24,5 +46,38 @@ namespace MyLocationsNote
         protected override void OnResume()
         {
         }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            //Services
+
+            /*containerRegistry.RegisterInstance<ISettinngsManager>(Container.Resolve<SettingsManager>());
+            containerRegistry.RegisterInstance<IRepository>(Container.Resolve<Repository>());
+            containerRegistry.RegisterInstance<IDbService>(Container.Resolve<DbService>());
+            containerRegistry.RegisterInstance<IRegistration>(Container.Resolve<Registration>());
+            containerRegistry.RegisterInstance<IAuthorization>(Container.Resolve<Authorization>());*/
+
+            //Navigation
+            /*containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<SignInPage, SignInPageViewModel>();
+            containerRegistry.RegisterForNavigation<SignUpPage, SignUpPageViewModel>();
+            containerRegistry.RegisterForNavigation<MainListView, MainListViewModel>();
+            containerRegistry.RegisterForNavigation<SettingPage, SettingPageViewModel>();
+            containerRegistry.RegisterForNavigation<AddEditProfilePage, AddEditProfileViewModel>();*/
+
+            // dialogs
+            //containerRegistry.RegisterDialog<ItemTappedDialog, ItemTappedDialogModel>();
+        }
+
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region --- Private helpers ---
+
+       
+        #endregion
     }
 }
