032da702cbc0b2e3859b4364c3f2eb8dbe895280
maximSologubov <95292495+maximSologubov@users.noreply.github.com>
2022-02-03 09:18:46 +0200

    add loginPage

---
+++ MyLocationsNote/MyLocationsNote/Services/Settings/ISettinngsManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLocationsNote.Services.Settings
+{
+    interface ISettinngsManager
+    {
+        bool DarkTheme { get; set; }
+    }
+}
---
+++ MyLocationsNote/MyLocationsNote/Services/Settings/SettingsManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Essentials;
+
+namespace MyLocationsNote.Services.Settings
+{
+    class SettingsManager
+    {
+        public bool DarkTheme
+        {
+            get => Preferences.Get(nameof(DarkTheme), false);
+            set => Preferences.Set(nameof(DarkTheme), value);
+        }
+    }
+}
